fcab10438aed860538b5166ef907f704c718fb91
JAFARNEJAD Sasan <0120524504@as28mm08.uni.lux>
2013-05-10 13:54:49 +0200

    hi

---
+++ JAPairhockey/JAPairhockey/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JAPairhockey
+//
+//  Created by JAFARNEJAD Sasan on 5/10/13.
+//  Copyright (c) 2013 JAFARNEJAD Sasan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ JAPairhockey/JAPairhockeyTests/JAPairhockeyTests.h
@@ -0,0 +1,13 @@
+//
+//  JAPairhockeyTests.h
+//  JAPairhockeyTests
+//
+//  Created by JAFARNEJAD Sasan on 5/10/13.
+//  Copyright (c) 2013 JAFARNEJAD Sasan. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface JAPairhockeyTests : SenTestCase
+
+@end
